334e5a0efddc5111c4fd5e3f67bbac7f282d64fe
mohsen-ameli <mohsen.ameli83@gmail.com>
2021-12-23 15:32:53 -0500

    idk

--- money_moe/settings.py
+++ money_moe/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if not DEBUG:
     ALLOWED_HOSTS = ['99.240.225.244', 'www.zarathustrust.com']
