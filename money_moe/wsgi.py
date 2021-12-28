d82b7ce1390818a21c2e6165f14c10826a0dc8ad
mohsen-ameli <mohsen.ameli83@gmail.com>
2021-12-28 17:02:09 -0500

    idk

--- money_moe/wsgi.py
+++ money_moe/wsgi.py
@@ -1,15 +1,37 @@
-import os
-import sys
+import os, site, sys
+
+site.addsitedir('/home/pi/money_moe/venv/lib/python3.7/site-packages')
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(BASE_DIR, '..'))
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "money_moe.settings"  # see footnote [2]
 
 from django.core.wsgi import get_wsgi_application
+_application = get_wsgi_application()
 
-sys.path.insert(0, '/home/pi/money_moe/users')
-sys.path.insert(0, '/home/pi/money_moe/accounts/') 
-sys.path.insert(0, '/home/pi/money_moe/money_moe/')
-sys.path.append('/home/pi/money_moe/venv/bin/python3')
+env_variables_to_pass = ['DB_NAME', 'DB_USER', 'DB_PASSWD', 'DB_HOST', ]
+def application(environ, start_response):
+    # pass the WSGI environment variables on through to os.environ
+    for var in env_variables_to_pass:
+        os.environ[var] = environ.get(var, '')
+    return _application(environ, start_response)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'money_moe.settings')
-os.environ['HTTPS'] = "on"
-os.environ['wsgi.url_scheme'] = 'https'
 
-application = get_wsgi_application()
+
+
+# import os
+# import sys
+
+# from django.core.wsgi import get_wsgi_application
+
+# sys.path.insert(0, '/home/pi/money_moe/users')
+# sys.path.insert(0, '/home/pi/money_moe/accounts/') 
+# sys.path.insert(0, '/home/pi/money_moe/money_moe/')
+# sys.path.append('/home/pi/money_moe/venv/bin/python3')
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'money_moe.settings')
+# os.environ['HTTPS'] = "on"
+# os.environ['wsgi.url_scheme'] = 'https'
+
+# application = get_wsgi_application()
