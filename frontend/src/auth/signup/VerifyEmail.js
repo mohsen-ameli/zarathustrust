bffc79bfcc61e462330e43bb22d228dc606c7090
mohsen-ameli <mohsen.ameli83@gmail.com>
2022-09-05 21:16:00 -0400

    removed phone verification

--- frontend/src/auth/signup/VerifyEmail.js
+++ frontend/src/auth/signup/VerifyEmail.js
@@ -6,7 +6,8 @@ const VerifyEmail = () => {
 
     return (
         <div className="verify-email">
-            <Otp url="/api/verify-email/" next="/verify-phone" statePrev={state} stateNew={{"fromVerifyEmail": true, "msg": "phone_verify"}} translationTitle="verify_email_title" translationHelp="email_verify" />
+            {/* <Otp url="/api/verify-email/" next="/verify-phone" statePrev={state} stateNew={{"fromVerifyEmail": true, "msg": "phone_verify"}} translationTitle="verify_email_title" translationHelp="email_verify" /> */}
+            <Otp url="/api/verify-email/" next="/verify-referral" statePrev={state} stateNew={{"fromVerifyPhone": true}} translationTitle="verify_email_title" translationHelp="email_verify" />
         </div>
     );
 }
